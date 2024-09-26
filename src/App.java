f0abf167ba652ad947e354e12a54215b2fa90241
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-09-26 13:52:42 -0400

    convert

--- src/App.java
+++ src/App.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class App {
+import java.time.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        LocalDateTime birthday = LocalDateTime.of(2001, 4, 2, 8, 30, 0);
+        ZoneId us = ZoneId.of("America/New_York");
+        ZonedDateTime birthdayZoned = ZonedDateTime.of(birthday, us);
+
+        ZoneId paris = ZoneId.of("Europe/Paris");
+        ZonedDateTime birthdayParis = birthdayZoned.withZoneSameInstant(paris);
+        System.out.println(birthdayZoned);
+        System.out.println(birthdayParis);
+
+
+
+
+    }
+
+
 }
